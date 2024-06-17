7c86c98a9d9fa123328a18216b4e2b73c3afbe91
ekmaldzaki <ekmalroyhan1010@gmail.com>
2024-06-17 22:40:16 +0700

    Add image analysis post

--- src/routes/routesPublic.js
+++ src/routes/routesPublic.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const { register, login } = require('../controllers/authController');
+const express = require("express");
+const { register, login } = require("../controllers/authController");
 
 const routerPublic = express.Router();
 
-routerPublic.post('/register', register);
-routerPublic.post('/login', login);
+routerPublic.post("/register", register);
+routerPublic.post("/login", login);
+router.post("/analyze-image", modelController.analyzeImage); //image analysis
 
 module.exports = routerPublic;
